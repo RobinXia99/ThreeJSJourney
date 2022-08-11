f96ec9d8c77e859f0f611eb9813244bbcfc029f7
RobinXia99 <>
2022-08-11 05:33:16 +0200

    raycaster, galaxy, hauntedhouse- lesson 20 checkpoint

--- src/App.js
+++ src/App.js
@@ -1,274 +1,295 @@
 
 import './App.css';
 import * as THREE from 'three'
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { Canvas, extend, useFrame, useLoader } from '@react-three/fiber';
-import { AmbientLight, Camera, Clock, HemisphereLightHelper, MeshStandardMaterial, PerspectiveCamera, PointLight, PointLightHelper, Vector3 } from 'three';
-import { Environment, OrbitControls, Plane, Text, Text3D, useHelper, useTexture } from '@react-three/drei';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Canvas, extend, useFrame, useLoader, useThree } from '@react-three/fiber';
+import { AmbientLight, BufferGeometry, Camera, Clock, HemisphereLightHelper, MeshStandardMaterial, PerspectiveCamera, PointLight, PointLightHelper, Vector3 } from 'three';
+import { Environment, OrbitControls, Plane, Stars, Text, Text3D, useCursor, useHelper, useTexture } from '@react-three/drei';
 import { folder, useControls } from 'leva';
+import HauntedHouse from './hauntedHouse';
 
-// function Text() {
 
-//   // const config = useMemo(() => {
-//   //   (
-//   //     {
-//   //       size: 0.5,
-//   //       height: 0.2,
-//   //       curveSegments: 12,
-//   //       bevelEnabled: true,
-//   //       bevelThickness: 0.03,
-//   //       bevelSize: 0.02,
-//   //       bevelOffset: 0,
-//   //       bevelSegments: 5
-//   //     }
 
-//   //   )
-//   // }, [])
+// function ThreeContent() {
+
+//   const [sphereRef, setSphereRef] = useState();
+//   const [lightRef, setLightRef] = useState();
+
 
 //   const matCap = useTexture('textures/whitematcap.jpeg');
 
-//   const textRef = useRef();
 
-//   const config = {
-//     size: 0.5,
-//     height: 0.2,
-//     curveSegments: 5,
-//     bevelEnabled: true,
-//     bevelThickness: 0.03,
-//     bevelSize: 0.02,
-//     bevelOffset: 0,
-//     bevelSegments: 4
-//   }
+//   const { color, roughness, metalness } = useControls({
+//     Material: folder({
+//       color: '#fff',
+//       roughness: {
+//         value: 0.7,
+//         min: 0,
+//         max: 1,
+//         step: 0.001
+//       },
+//       metalness: {
+//         value: 0,
+//         min: 0,
+//         max: 1,
+//         step: 0.001
+//       }
+//     })
+
+//   })
+
+
+//   // useFrame((state) => {
+
+//   //   let clock = state.clock.getElapsedTime();
+
+//   //   if(sphereRef) {
+//   //     sphereRef.rotation.y = 0.1 * clock;
+//   //     sphereRef.rotation.x = 0.15 * clock;
+//   //   }
+
+//   // })
+
+//   useFrame((state) => {
+
+//     if(sphereRef) {
+//       sphereRef.position.x = Math.cos(state.clock.getElapsedTime()) * 1.5
+//       sphereRef.position.z = Math.sin(state.clock.getElapsedTime()) * 1.5
+//       sphereRef.position.y = Math.abs(Math.sin(state.clock.getElapsedTime() * 3))
+//     }
+
+//   })
+
+
+
+//   return (
+//     <>
+
+
+//       <mesh ref={setSphereRef} castShadow>
+//         <sphereBufferGeometry args={[0.5, 64, 64]} />
+//         <meshStandardMaterial color={color} roughness={roughness} metalness={metalness}/>
+//       </mesh>
+
+
+//       <ambientLight intensity={0.3} color={0xffffff} />
+
+//       <directionalLight
+//       shadow-radius={10}
+//       shadow-camera-top={3.5}
+//       shadow-camera-right={2}
+//       shadow-camera-left={- 2}
+//       shadow-camera-bottom={ -2}
+//       shadow-camera-far={8}
+//       shadow-camera-near={0}
+//       ref={setLightRef}
+//       shadow-mapSize-height={1024}
+//       shadow-mapSize-width={1024 }
+//       castShadow
+//       intensity={0.5}
+//       color={0xffffff}
+//       position={[2, 2, -1]}/>
+
+//       {/* {lightRef && <directionalLightHelper args={[lightRef]}/>} */}
+//       {lightRef && <cameraHelper args={[lightRef.shadow.camera]}/>}
+
+//       <OrbitControls />
+
+//     </>
+//   )
+
+// }
+
+function Lightning() {
+
+  return (
+    <>
+      <ambientLight args={["#b9d5ff", 0.12]} />
+      <OrbitControls />
+    </>
+  )
+}
+
+// function Particles() {
+
+//   const count = 6000;
+
+//   const positions = new Float32Array(count);
+//   const colors = new Float32Array(count);
+
+//   const particleRef = useRef();
+
+
+//   const particleTextures = useTexture({
+//     map: '/textures/magic_05.png'
+//   })
+
 
 //   useEffect(() => {
-//     textRef.current.geometry.center()
-    
-//     // textRef.current.geometry.translate(
-//     //   - (textRef.current.geometry.boundingBox.max.x - 0.02) * 0.5,
-//     //   - (textRef.current.geometry.boundingBox.max.y - 0.02) * 0.5,
-//     //   - (textRef.current.geometry.boundingBox.max.z -0.03) * 0.5,
 
-//     // )
-//   }, [textRef.current])
 
-  
+
+//     for(let i = 0; i < count; i++) {
+//       positions[i] = (Math.random() - 0.5) * 10
+//       colors[i] = Math.random();
+//     }
+
+//     particleRef.current.setAttribute(
+//       'position',
+//       new THREE.BufferAttribute(positions, 3)
+//     )
+
+//     particleRef.current.setAttribute(
+//       'color',
+//       new THREE.BufferAttribute(colors, 3)
+//     )
+
+
+//   },[particleRef.current])
+
+
 
 //   return (
-//     <>
-//       {/* <mesh>
-//         <boxBufferGeometry args={[1, 1, 1, 2, 2, 2]} />
-//         <meshBasicMaterial />
-//       </mesh> */}
-//       <Text3D ref={textRef} font={'fonts/helvetiker_regular.typeface.json'} {...config}>
-//         Hello Three.js
-//         <meshMatcapMaterial matcap={matCap}/>
-//       </Text3D>
-
-//     </>
-
+//     <points>
+//       <bufferGeometry ref={particleRef}/>
+//       <pointsMaterial
+//       size={0.1}
+//       sizeAttenuation
+//       alphaMap={particleTextures.map}
+//       map={particleTextures.map}
+//       transparent
+//       depthWrite={false}
+//       vertexColors/>
+//     </points>
 //   )
 
 // }
 
 
+function RaycasterTesting() {
+
+  const object1 = useRef();
+  const object2 = useRef();
+  const object3 = useRef();
+  const raycastRef = useRef();
+
+  const racyastOrigin = new THREE.Vector3(- 3, 0, 0);
+  const racyastDirection = new THREE.Vector3(10, 0, 0).normalize();
+
+  const [hovered1, setHovered1] = useState(false);
+  const [hovered2, setHovered2] = useState(false);
+  const [hovered3, setHovered3] = useState(false);
+
+  useCursor(hovered1 || hovered2 || hovered3);
+
+  // const pointer = new THREE.Vector2();
+
+  // window.addEventListener('mousemove', (event) => {
+
+  //   pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  //   pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+  // });
 
 
-
-
-// function LimestoneTexture() {
-
-
-//   // const textures = useTexture({
-//   //   map: '/textures/limestone-cliffs_albedo.png',
-//   //   displacementMap: '/textures/limestone-cliffs_height.png',
-//   //   aoMap: '/textures/limestone-cliffs_ao.png',
-//   //   roughnessMap: '/textures/limestone-cliffs_roughness.png',
-//   //   metalnessMap: '/textures/limestone-cliffs_metallic.png',
-//   //   normalMap: '/textures/limestone-cliffs_normal-ogl.png'
-//   // })
-
-//   // const { metalness, roughness, wireframe, aoMapIntensity, displacementScale } = useControls("Material", {
-//   //   metalness: {
-//   //     value: 0.7,
-//   //     min: 0,
-//   //     max: 1,
-//   //     step: 0.0001
-//   //   },
-//   //   roughness: {
-//   //     value: 0.2,
-//   //     min: 0,
-//   //     max: 1,
-//   //     step: 0.0001
-//   //   },
-//   //   wireframe: false,
-//   //   texture: folder({
-//   //     aoMapIntensity: {
-//   //       value: 0.5,
-//   //       min: 0,
-//   //       max: 10,
-//   //       step: 0.0001
-//   //     },
-//   //     displacementScale: {
-//   //       value: 0.3,
-//   //       min: 0,
-//   //       max: 10,
-//   //       step: 0.0001
-//   //     }
-//   //   })
-
-//   // })
-
-
-
-//   // {...textures} displacementScale={displacementScale} aoMapIntensity={aoMapIntensity}
-
-//   return (
-//     <>
-//     <meshStandardMaterial metalness={metalness} roughness={roughness} wireframe={wireframe}  />
-//     </>
-
-//   )
-
-// }
-
-function ThreeContent() {
-
-  const [lightRef, setLightRef] = useState();
-
-  const rectLightRef = useRef();
-
-  const matCap = useTexture('textures/whitematcap.jpeg');
-
-  const sphereRef = useRef();
-  const boxRef = useRef();
-  const torusRef = useRef();
-
-  const { color, roughness, metalness } = useControls({
-    Material: folder({
-      color: '#fff',
-      roughness: {
-        value: 1,
-        min: 0,
-        max: 1,
-        step: 0.001
-      },
-      metalness: {
-        value: 0,
-        min: 0,
-        max: 1,
-        step: 0.001
-      }
-    })
-    
-  })
 
 
   useFrame((state) => {
 
-    let clock = state.clock.getElapsedTime();
 
-    if(sphereRef.current) {
-      sphereRef.current.rotation.y = 0.1 * clock;
-      sphereRef.current.rotation.x = 0.15 * clock;
-    }
-    if(boxRef.current) {
-      boxRef.current.rotation.y = 0.1 * clock;
-      boxRef.current.rotation.x = 0.15 * clock;
-    }
-    if(torusRef.current) {
-      torusRef.current.rotation.y = 0.1 * clock;
-      torusRef.current.rotation.x = 0.15 * clock;
+    
+    
+
+    if (object1.current && object2.current && object3.current && raycastRef.current) {
+
+      // raycastRef.current.setFromCamera(pointer, state.camera)
+
+      // //Change color depending on intersections
+      // const objectArray = [object1.current, object2.current, object3.current];
+
+      // const intersections = raycastRef.current.intersectObjects(objectArray);
+
+      // object1.current.position.y = Math.sin(state.clock.getElapsedTime()) * 1.5;
+      // object2.current.position.y = Math.sin(state.clock.getElapsedTime()) * 1;
+      // object3.current.position.y = Math.sin(state.clock.getElapsedTime()) * 2;
+
+      // for(const object of objectArray) {
+      //   object.material.color.set("#ff0000")
+      // }
+
+      // for(const intersection of intersections) {
+      //   intersection.object.material.color.set("#0000ff")
+      // }
+
+  
+
+
+
     }
 
   })
 
-  // useEffect(() => {
-  //   sphereRef.current.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphereRef.current.geometry.attributes.uv.array, 2))
+  useEffect(() => {
 
-  //   console.log("hi")
-  // },[sphereRef])
+    console.log(raycastRef.current.intersectObjects([object1.current, object2.current, object3.current]))
 
-  // useEffect(() => {
-  //   planeRef.current.geometry.setAttribute('uv2', new THREE.BufferAttribute(planeRef.current.geometry.attributes.uv.array, 2))
-
-  // },[planeRef])
-
-  // useEffect(() => {
-  //   torusRef.current.geometry.setAttribute('uv2', new THREE.BufferAttribute(torusRef.current.geometry.attributes.uv.array, 2))
-
-  // },[torusRef])
-
-  // function createParticles() {
-  //   let particles = [];
-
-  //   for(let i = 0; i < 250; i++) {
-  //     let x = (Math.random() - 0.5) * 10;
-  //     let y = (Math.random() - 0.5) * 10;
-  //     let z = (Math.random() - 0.5) * 10;
-  //     particles[i] = [x, y ,z]; 
-  //   }
-  //   return particles;
-  // }
-
-  // const particles = createParticles().map((pos, i) => (
-    
-  //     <mesh key={i}  position={pos} rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]} scale={Math.random() - 0.5} >
-  //       <octahedronBufferGeometry args={[0.5]} />
-  //       <meshMatcapMaterial matcap={matCap}/>
-  //     </mesh>
-    
-    
-  // ));
+  }, [raycastRef])
 
   useEffect(() => {
-    rectLightRef.current.lookAt(new THREE.Vector3())
-  },[rectLightRef.current])
 
+
+  }, [])
 
 
   return (
     <>
 
-      <mesh ref={sphereRef} position={[-1.5, 0, 0]}>
-        <sphereBufferGeometry args={[0.5, 64, 64]} />
-        <meshStandardMaterial color={color} roughness={roughness} metalness={metalness} />
+      <raycaster ref={raycastRef} args={[racyastOrigin, racyastDirection]} />
+
+      <mesh
+      position-x={-2}
+      ref={object1}
+      onPointerEnter={(e) => {
+        setHovered1(true)
+        e.stopPropagation()
+      }}
+      onPointerLeave={() => setHovered1(false)}>
+        <sphereBufferGeometry args={[0.5, 16, 16]} />
+        <meshBasicMaterial color={(hovered1) ?  "#00ff00" : "#ff0000"} />
       </mesh>
 
-      <mesh ref={boxRef}>
-        <boxBufferGeometry args={[0.65, 0.65, 0.65, 2, 2, 2]} do />
-        <meshStandardMaterial color={color} roughness={roughness} metalness={metalness}/>
+      <mesh
+      ref={object2}
+      onPointerEnter={(e) => {
+        setHovered2(true)
+        e.stopPropagation()
+      }}
+      onPointerLeave={() => setHovered2(false)}>
+        <sphereBufferGeometry args={[0.5, 16, 16]} />
+        <meshBasicMaterial color={(hovered2) ? "#00ff00" : "#ff0000"} />
       </mesh>
 
-      <mesh ref={torusRef} position={[1.5, 0, 0]}>
-        <torusBufferGeometry args={[0.3, 0.2, 64,  128]} />
-        <meshStandardMaterial color={color} roughness={roughness} metalness={metalness}/>
+      <mesh
+      position-x={2}
+      ref={object3}
+      onPointerEnter={(e) => {
+        setHovered3(true)
+        e.stopPropagation()
+      }}
+      onPointerLeave={() => setHovered3(false)}>
+        <sphereBufferGeometry args={[0.5, 16, 16]} />
+        <meshBasicMaterial color={(hovered3) ? "#00ff00" : "#ff0000"} />
       </mesh>
-
-      <ambientLight intensity={0.3} color={0xffffff} />
-      <hemisphereLight intensity={0.3} color={0xff0000} groundColor={0x0000ff}/>
-      
-      <directionalLight ref={setLightRef} intensity={0.3} color={0x00fffc} position={[1, 0.25, 0]}/>
-      {lightRef && <directionalLightHelper args={[lightRef, 0.3]}/>}
-
-      <pointLight intensity={0.5} color={0xff9000} position={[1, - 0.5, 1]} distance={3}/>
-
-      <rectAreaLight ref={rectLightRef} intensity={5} color={0x4e00ff} width={1} height={1} position={[- 1.5, 0, 1.5]}/>
-      
-      <spotLight position={[0, 2, 3]} intensity={1} distance={6} color={0x78ff00} angle={Math.PI * 0.1} penumbra={0.25} decay={1}/>
-
-      {/* <pointLight ref={lightRef} position={[2, 3, 4]} intensity={0.5} /> */}
-      <OrbitControls />
-      {/* <Environment files={'/chinese_garden_4k.hdr'} background={true}/> */}
-      {/* {particles} */}
 
     </>
   )
 
 }
 
+
+
+
 function App() {
-
-
 
   const viewport = {
     width: window.innerWidth,
@@ -276,18 +297,29 @@ function App() {
   };
 
 
-
   return (
     <div className="App">
       {/* <RedCube></RedCube> */}
 
-      <Canvas className='webgl' camera={{ position: [0, 0, 3], fov: 75, aspect: viewport.width / viewport.height, near: 0.1, far: 150 }}>
+      <Canvas
+        className='webgl'
+        camera={{ position: [0, 1, 5], fov: 50, aspect: viewport.width / viewport.height, near: 0.1, far: 150 }}
+      >
+
         {/* <ReactCube /> */}
-        <mesh rotation-x={-Math.PI / 2} position-y={-1}>
-          <planeBufferGeometry args={[10, 10, 2, 2]}/>
-          <meshStandardMaterial roughness={0.45} metalness={0.7}/>
+        {/* <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
+          <planeBufferGeometry args={[7, 7, 2, 2]}/>
+          <meshStandardMaterial roughness={1}/>
         </mesh>
-        <ThreeContent />
+        <ThreeContent /> */}
+        {/* <Stars radius={50} depth={50} count={1000} factor={4} saturation={0} fade speed={1} /> */}
+        {/* <HauntedHouse/> */}
+
+        <Lightning />
+        <RaycasterTesting />
+        {/* <fog attach={'fog'} args={['#262837', 1, 15]}/>
+        <color attach="background" args={["#262837"]}/> */}
+
       </Canvas>
 
 
---
+++ src/galaxy.js
@@ -0,0 +1,152 @@
+function Galaxy() {
+
+    const galaxyRef = useRef();
+  
+    const { 
+      ParticleCount,
+      ParticleSize,
+      GalaxyRadius,
+      GalaxyBranches,
+      Spin,
+      Randomness,
+      RandomnessPower,
+      InsideColor,
+      OutsideColor } = useControls({
+  
+      ParticleCount: {
+  
+        value: 8000,
+        min: 1000,
+        max: 100000,
+        step: 100,
+      },
+      ParticleSize: {
+  
+        value: 0.02,
+        min: 0.001,
+        max: 0.1,
+        step: 0.001
+      },
+      GalaxyRadius: {
+  
+        value: 5,
+        min: 1,
+        max: 10,
+        step: 1
+      },
+      GalaxyBranches: {
+  
+        value: 3,
+        min: 2,
+        max: 20,
+        step: 1
+      },
+      Spin: {
+  
+        value: 1,
+        min: -5,
+        max: 5,
+        step: 0.01
+      },
+      Randomness: {
+  
+        value: 0.2,
+        min: 0,
+        max: 2,
+        step: 0.001
+      },
+      RandomnessPower: {
+        
+        value: 3,
+        min: 1,
+        max: 10,
+        step: 0.001
+      },
+      InsideColor: "#ff6030",
+      OutsideColor: "#1b3984"
+  
+    })
+  
+    // const positions = useMemo(
+  
+    //   () => new Float32Array(generateParticles(count))
+  
+    // ,[])
+  
+    const positions = new Float32Array(ParticleCount * 3)
+    const colors = new Float32Array(ParticleCount * 3)
+  
+  
+  
+    useEffect(() => {
+  
+      const colorInside = new THREE.Color(InsideColor);
+      const colorOutside = new THREE.Color(OutsideColor);
+  
+      for (let i = 0; i < ParticleCount; i++) {
+  
+        const i3 = i * 3;
+  
+        const radius = Math.random() * GalaxyRadius;
+        const branchAngle = (i % GalaxyBranches) / GalaxyBranches * Math.PI * 2;
+        const spinAngle = radius * Spin
+  
+        const randomX = Math.pow(Math.random(), RandomnessPower) * (Math.random() < 0.5 ? 1 : - 1) 
+        const randomY = Math.pow(Math.random(), RandomnessPower) * (Math.random() < 0.5 ? 1 : - 1)
+        const randomZ = Math.pow(Math.random(), RandomnessPower) * (Math.random() < 0.5 ? 1 : - 1)
+  
+  
+        positions[i3    ] = Math.cos(branchAngle + spinAngle) * radius + randomX
+        positions[i3 + 1] = randomY
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
+  
+        const mixedColor = colorInside.clone();
+  
+        mixedColor.lerp(colorOutside, radius / GalaxyRadius)
+  
+        colors[i3    ] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+  
+      }
+  
+      galaxyRef.current.geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+      galaxyRef.current.geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+  
+      console.log('attributes set')
+  
+  
+  
+    }, [galaxyRef.current, ParticleCount, GalaxyBranches, GalaxyRadius, Spin, Randomness, RandomnessPower, InsideColor, OutsideColor])
+  
+    return (
+      <>
+  
+        <points ref={galaxyRef}>
+  
+          <bufferGeometry>
+  
+            {/* <bufferAttribute
+              attachObject={['attributes', 'position']}
+              count={count}
+              array={positions}
+              itemSize={3}
+            /> */}
+  
+          </bufferGeometry>
+  
+          <pointsMaterial
+            size={ParticleSize}
+            sizeAttenuation
+            depthWrite={false}
+            blending={THREE.AdditiveBlending}
+            vertexColors/>
+        </points>
+  
+      </>
+  
+    )
+  
+  }
+
+  export default Galaxy;
